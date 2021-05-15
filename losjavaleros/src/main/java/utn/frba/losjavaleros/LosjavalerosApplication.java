7239a43e39c1f89b049d0e5e007de3fde0682a7d
Fabrizio <fabrizio>
2021-05-15 19:50:18 -0300

    scaffolding

---
+++ losjavaleros/src/main/java/utn/frba/losjavaleros/LosjavalerosApplication.java
@@ -0,0 +1,13 @@
+package utn.frba.losjavaleros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LosjavalerosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LosjavalerosApplication.class, args);
+    }
+
+}
---
+++ losjavaleros/src/test/java/utn/frba/losjavaleros/LosjavalerosApplicationTests.java
@@ -0,0 +1,13 @@
+package utn.frba.losjavaleros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LosjavalerosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
