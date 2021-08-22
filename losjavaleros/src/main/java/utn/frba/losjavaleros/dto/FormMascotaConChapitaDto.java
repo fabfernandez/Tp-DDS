8fec756f33f98053995794a751a6e5c7f002f5b6
leonardobritez <britez20008@gmail.com>
2021-08-21 23:40:02 -0300

    formulario encontrar mascota CON CHAPITA

---
+++ losjavaleros/src/main/java/utn/frba/losjavaleros/dto/FormMascotaConChapitaDto.java
@@ -0,0 +1,26 @@
+package utn.frba.losjavaleros.dto;
+
+public class FormMascotaConChapitaDto {
+	
+	private MascotaDto mascotaDto;
+	private UsuarioDto usuarioDto;
+	
+//	Nombre y apellido
+//	● Fecha de nacimiento
+//	● Tipo y nro de documento
+//	● Dirección
+//	● Al menos un dato de contacto:
+//	o Nombre y apellido
+//	o Teléfono
+//	o Email
+//	o Formas de notificación (email otro medio)
+//	
+//	▪ Datos sobre la mascota perdida:
+//	● Al menos una foto
+//	● Descripción del estado en el cual la encontró
+//	● Lugar donde la encontró, seleccionable a través de un
+//	mapa
+
+	
+	
+}
