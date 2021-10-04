912f0c1c087f8a710eeb0e23d0458533a4f79c1d
Fabrizio Fernandez <fabrizio.fernandez@mercadolibre.com>
2021-10-04 16:08:12 -0300

    app levanta

--- losjavaleros/src/main/java/utn/frba/losjavaleros/services/MascotaService.java
+++ losjavaleros/src/main/java/utn/frba/losjavaleros/services/MascotaService.java
@@ -14,71 +14,71 @@ import utn.frba.losjavaleros.repository.MascotaRepository;
 @Service
 public class MascotaService {
 
-    @Autowired
-    private MascotaRepository mascotaRepository;
+  @Autowired
+  private MascotaRepository mascotaRepository;
 
-    @Autowired
-    private EnviadorDeMails enviadorDeMails;
+  @Autowired
+  private EnviadorDeMails enviadorDeMails;
 
-    public Mascota crearMascota(final MascotaDto mascotaDto, final Usuario duenio) {
+  public Mascota crearMascota(final MascotaDto mascotaDto, final Usuario duenio) {
 
 
-        //Traer caracteristicas de base de datos por id
-        Caracteristica caracteristicaDeBaseDeDatos = new Caracteristica("Color principal", "input");
+    //Traer caracteristicas de base de datos por id
+    Caracteristica caracteristicaDeBaseDeDatos = new Caracteristica(420L, "Color principal", "input");
 
-        //insertar respuestas del usuario
-        List<CaracteristicaCompleta> caracteristicas =
-                List.of(new CaracteristicaCompleta(caracteristicaDeBaseDeDatos, "Rosa"));
+    //insertar respuestas del usuario
+    List<CaracteristicaCompleta> caracteristicas =
+        List.of(new CaracteristicaCompleta(caracteristicaDeBaseDeDatos.getId(), caracteristicaDeBaseDeDatos, "Rosa"));
 
-        //TODO crear fotos???
+    //TODO crear fotos???
 
-        //crear la mascota
-        Mascota mascota = new Mascota(
-                1, //TODO ESTE VALOR DEBE SER AUTO INCREMENTAL.
-                duenio,
-                caracteristicas,
-                UUID.randomUUID().toString(),
-                mascotaDto.getTipo(),
-                mascotaDto.getNombre(),
-                mascotaDto.getApodo(),
-                mascotaDto.getEdad(),
-                Sexo.valueOf(mascotaDto.getSexo()),
-                mascotaDto.getDescripcion(),
-                null, MascotaEstadoEnum.valueOf(mascotaDto.getEstado()));
+    //crear la mascota
+    Mascota mascota = new Mascota(
+        1, //TODO ESTE VALOR DEBE SER AUTO INCREMENTAL.
+        duenio,
+        caracteristicas,
+        UUID.randomUUID().toString(),
+        mascotaDto.getTipo(),
+        mascotaDto.getNombre(),
+        mascotaDto.getApodo(),
+        mascotaDto.getEdad(),
+        Sexo.valueOf(mascotaDto.getSexo()),
+        mascotaDto.getDescripcion(),
+        null, MascotaEstadoEnum.valueOf(mascotaDto.getEstado()));
 
-        //guardar mascota
-        mascotaRepository.guardar(mascota);
-        duenio.getMascotas().add(mascota);
+    //guardar mascota
+    mascotaRepository.guardar(mascota);
+    duenio.getMascotas().add(mascota);
 
-        return mascota;
-    }
+    return mascota;
+  }
 
-    public List<Mascota> filtrarMascotas(String estado) {
-        return mascotaRepository.getMascotasEstado(estado);
-    }
+  public List<Mascota> filtrarMascotas(String estado) {
+    return mascotaRepository.getMascotasEstado(estado);
+  }
 
-    public void notificarSobreMascotaEncontrada(final FormMascotaConChapitaDto formulario, final int mascotaId) {
-        /*
-         * Una vez completado el formulario, el sistema deberá notificarle de esta situación al dueño
-         * (conocido por estar ligado a la chapita) mediante los medios de notificación preferidos.*/
-        Mascota mascota = mascotaRepository.getMascotaById(mascotaId);
-        String nombreMascota = mascota.getNombre();
+  public void notificarSobreMascotaEncontrada(final FormMascotaConChapitaDto formulario, final int mascotaId) {
+    /*
+     * Una vez completado el formulario, el sistema deberá notificarle de esta situación al dueño
+     * (conocido por estar ligado a la chapita) mediante los medios de notificación preferidos.*/
+    Mascota mascota = mascotaRepository.getMascotaById(mascotaId);
+    String nombreMascota = mascota.getNombre();
 
-        Usuario duenio = mascota.getDuenio();
-        String email = duenio.getEmail();
-        String nombreDuenio = duenio.getNombre();
+    Usuario duenio = mascota.getDuenio();
+    String email = duenio.getEmail();
+    String nombreDuenio = duenio.getNombre();
 
-        UsuarioDto rescatista = formulario.getUsuarioDto();
-        String nombreRescatista = rescatista.getNombreUsuario();
-        String apellidoRescatista = rescatista.getApellido();
+    UsuarioDto rescatista = formulario.getUsuarioDto();
+    String nombreRescatista = rescatista.getNombreUsuario();
+    String apellidoRescatista = rescatista.getApellido();
 
-        String cuerpoEmail = String.format("Hola %s, una persona llamada %s %s encontró a tu mascota %s",
-                nombreDuenio, nombreRescatista, apellidoRescatista, nombreMascota);
-        enviadorDeMails.enviarMail(email, "Alguien encontro a tu mascota!", cuerpoEmail);
-    }
+    String cuerpoEmail = String.format("Hola %s, una persona llamada %s %s encontró a tu mascota %s",
+        nombreDuenio, nombreRescatista, apellidoRescatista, nombreMascota);
+    enviadorDeMails.enviarMail(email, "Alguien encontro a tu mascota!", cuerpoEmail);
+  }
 
-    public Mascota getMascotaById(int id) {
-        return mascotaRepository.getMascotaById(id);
-    }
+  public Mascota getMascotaById(int id) {
+    return mascotaRepository.getMascotaById(id);
+  }
 
 }
---
+++ losjavaleros/src/test/java/utn/frba/losjavaleros/entrega1punto1.java
@@ -0,0 +1,18 @@
+package utn.frba.losjavaleros;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class entrega1punto1 {
+
+
+  @BeforeAll
+  void setUp(){
+
+  }
+
+  @Test
+  void test1(){
+
+  }
+}
